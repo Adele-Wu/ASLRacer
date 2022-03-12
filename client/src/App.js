32d24c4e97fa501902a3a89e198834ccd0d89b0c
EJ Cenario <kurtina09@yahoo.com>
2022-03-12 14:09:36 -0800

    navbar(no styling)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './components/SignUp';
 import Game from './pages/Game';
@@ -9,6 +10,7 @@ import Game from './pages/Game';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
