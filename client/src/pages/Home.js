4699f72867830f80da05d215c2661b9b9e24c4ac
kennethchuson <kennethchuson@yahoo.com>
2022-03-11 23:50:38 -0800

    added home image

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import Game from './Game'
 
 function Home() {
   let history = useHistory() 
@@ -9,7 +8,9 @@ function Home() {
         <h1>Home Page</h1> 
         <button onClick={() => history.push("/login")}>Login In</button>
         <button onClick={() => history.push("/signup")}>Sign Up</button>
-        <button onClick={() => history.push("/game")}>Play</button> 
+        <button onClick={() => history.push("/game")}>Play</button>
+        <img src="https://cdn.discordapp.com/attachments/751224555267162193/952110054381875250/advantage-trophy-logo.png" />
+        <h1>High Scroes</h1>
 
     </div>
   )
