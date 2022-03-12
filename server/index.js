e20d92e04833357706e2d50c5c928b4503a1535e
kennethchuson <kennethchuson@yahoo.com>
2022-03-11 19:29:07 -0800

    set up server register and login

--- server/index.js
+++ server/index.js
@@ -1,8 +1,79 @@
 const express = require('express')
+const mysql = require("mysql");
+const cors = require("cors");
+
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const bcrypt = require("bcrypt");
+
+
 const app = express()
 const port = 3000
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
+
+
+// const db = mysql.createConnection({
+//   user: "root",
+//   host: "localhost",
+//   password: "password",
+//   database: "LoginSystem",
+// });
+
+
+
+//get 
+
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
+
+//post
+
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  
+});
 
 
 
